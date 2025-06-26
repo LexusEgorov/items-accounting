a441de6f426011c8ffdc13db1b4d05959696e71b
lexusegorov <alexey.egorov.job@gmail.com>
2025-06-27 01:37:52 +0700

    feat: add services

--- internal/services/categories/categories.go
+++ internal/services/categories/categories.go
@@ -1,37 +1,88 @@
 package categories
 
-import "github.com/LexusEgorov/items-accounting/internal/models"
+import (
+	"context"
+
+	"github.com/LexusEgorov/items-accounting/internal/models"
+	"github.com/LexusEgorov/items-accounting/internal/utils"
+)
 
 type Storager interface {
-	Add(string) (models.Category, error)
-	Get(int) (models.Category, error)
-	Set(int, string) (models.Category, error)
-	Delete(int) error
+	Add(ctx context.Context, name string) (id int, err error)
+	Get(ctx context.Context, id int) (category models.Category, err error)
+	Set(ctx context.Context, id int, name string) error
+	Delete(ctx context.Context, id int) error
 }
 
 type Categories struct {
 	storage Storager
 }
 
-//TODO: add validate data (for example)
 func New(storage Storager) *Categories {
 	return &Categories{
 		storage: storage,
 	}
 }
 
-func (c Categories) Add(name string) (models.Category, error) {
-	return c.storage.Add(name)
+func (c Categories) Add(ctx context.Context, name string) (models.CategoryDTO, error) {
+	errPrefix := "service.Categories.Add"
+
+	//TODO: move to validateFn
+	if name == "" {
+		return models.CategoryDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("name"))
+	}
+
+	id, err := c.storage.Add(ctx, name)
+
+	if err != nil {
+		return models.CategoryDTO{}, utils.GetError(errPrefix, err)
+	}
+
+	return models.CategoryDTO{
+		ID:   id,
+		Name: name,
+	}, nil
 }
 
-func (c Categories) Set(ID int, name string) (models.Category, error) {
-	return c.storage.Set(ID, name)
+func (c Categories) Set(ctx context.Context, category models.CategoryDTO) (models.CategoryDTO, error) {
+	errPrefix := "service.Categories.Set"
+
+	//TODO: move to validateFn
+	if category.Name == "" {
+		return models.CategoryDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("name"))
+	}
+
+	if category.ID == 0 {
+		return models.CategoryDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("id"))
+	}
+
+	err := c.storage.Set(ctx, category.ID, category.Name)
+
+	if err != nil {
+		return models.CategoryDTO{}, utils.GetError(errPrefix, err)
+	}
+
+	return category, nil
 }
 
-func (c Categories) Get(ID int) (models.Category, error) {
-	return c.storage.Get(ID)
+func (c Categories) Get(ctx context.Context, ID int) (models.CategoryDTO, error) {
+	errPrefix := "service.Categories.Get"
+	category, err := c.storage.Get(ctx, ID)
+
+	if err != nil {
+		return models.CategoryDTO{}, utils.GetError(errPrefix, err)
+	}
+
+	return category.ToDTO(), nil
 }
 
-func (c Categories) Delete(ID int) error {
-	return c.storage.Delete(ID)
+func (c Categories) Delete(ctx context.Context, id int) error {
+	errPrefix := "service.Categories.Delete"
+
+	//TODO: move to validateFn
+	if id <= 0 {
+		return utils.GetError(errPrefix, models.NewEmptyErr("id"))
+	}
+
+	return utils.GetError(errPrefix, c.storage.Delete(ctx, id))
 }
--- internal/services/products/products.go
+++ internal/services/products/products.go
@@ -1,37 +1,98 @@
 package products
 
-import "github.com/LexusEgorov/items-accounting/internal/models"
+import (
+	"context"
+
+	"github.com/LexusEgorov/items-accounting/internal/models"
+	"github.com/LexusEgorov/items-accounting/internal/utils"
+)
 
 type Storager interface {
-	Add(string) (models.Product, error)
-	Get(int) (models.Product, error)
-	Set(models.Product) (models.Product, error)
-	Delete(int) error
+	Add(ctx context.Context, product models.ProductDTO) (id int, err error)
+	Get(ctx context.Context, id int) (product models.Product, err error)
+	Set(ctx context.Context, product models.ProductDTO) error
+	Delete(ctx context.Context, id int) error
 }
 
 type Products struct {
 	storage Storager
 }
 
-//TODO: add validate data (for example)
 func New(storage Storager) *Products {
 	return &Products{
 		storage: storage,
 	}
 }
 
-func (c Products) Add(name string) (models.Product, error) {
-	return c.storage.Add(name)
+func (c Products) Add(ctx context.Context, product models.ProductDTO) (models.ProductDTO, error) {
+	errPrefix := "service.Products.Add"
+
+	//TODO: move to validateFn
+	if product.CatID == 0 {
+		return models.ProductDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("categoryId"))
+	}
+
+	//TODO: move to validateFn
+	if product.Name == "" {
+		return models.ProductDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("name"))
+	}
+
+	id, err := c.storage.Add(ctx, product)
+
+	if err != nil {
+		return models.ProductDTO{}, utils.GetError(errPrefix, err)
+	}
+
+	product.ID = id
+	return product, nil
 }
 
-func (c Products) Set(product models.Product) (models.Product, error) {
-	return c.storage.Set(product)
+func (c Products) Set(ctx context.Context, product models.ProductDTO) (models.ProductDTO, error) {
+	errPrefix := "service.Products.Set"
+
+	//TODO: move to validateFn
+	if product.CatID <= 0 {
+		return models.ProductDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("categoryId"))
+	}
+
+	//TODO: move to validateFn
+	if product.Name == "" {
+		return models.ProductDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("name"))
+	}
+
+	err := c.storage.Set(ctx, product)
+
+	if err != nil {
+		return models.ProductDTO{}, utils.GetError(errPrefix, err)
+	}
+
+	return product, nil
 }
 
-func (c Products) Get(ID int) (models.Product, error) {
-	return c.storage.Get(ID)
+func (c Products) Get(ctx context.Context, id int) (models.ProductDTO, error) {
+	errPrefix := "service.Products.Get"
+
+	//TODO: move to validateFn
+	if id <= 0 {
+		return models.ProductDTO{}, utils.GetError(errPrefix, models.NewEmptyErr("id"))
+	}
+
+	product, err := c.storage.Get(ctx, id)
+
+	if err != nil {
+		return models.ProductDTO{}, utils.GetError(errPrefix, err)
+	}
+
+	return product.ToDTO(), nil
 }
 
-func (c Products) Delete(ID int) error {
-	return c.storage.Delete(ID)
+func (c Products) Delete(ctx context.Context, id int) error {
+	errPrefix := "service.Products.Delete"
+
+	//TODO: move to validateFn
+	if id <= 0 {
+		return utils.GetError(errPrefix, models.NewEmptyErr("id"))
+	}
+
+	return utils.GetError(errPrefix, c.storage.Delete(ctx, id))
 }
