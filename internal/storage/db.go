c1c6821251b419369250e6ee19bc95194d489979
lexusegorov <alexey.egorov.job@gmail.com>
2025-07-15 19:22:57 +0700

    refactor: return back db

--- internal/storage/db.go
+++ internal/storage/db.go
@@ -4,22 +4,13 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/LexusEgorov/items-accounting/internal/config"
 )
 
-type Storager interface {
-	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
-	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
-	Ping(ctx context.Context) error
-	Close()
-}
-
 type DB struct {
-	DB Storager
+	DB *pgxpool.Pool
 }
 
 func NewDB(cfg config.DBConfig) (*DB, error) {
